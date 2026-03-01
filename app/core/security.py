ae44b7c697b43c183f2b98406b11bfc463787c68
crispr <cbrentas@gmail.com>
2026-03-01 22:04:30 +0000

    initial commit (fixed some things, added readme fixed gitignore)

--- app/core/security.py
+++ app/core/security.py
@@ -2,19 +2,23 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 import secrets
 
+from app.core.settings import settings
+
 security = HTTPBasic()
 
-# Replace later with real auth system
-USERS = {
-    "w0nts4y": "v3ryc00l"
-}
-
 def basic_auth(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_password = USERS.get(credentials.username)
 
-    if not correct_password or not secrets.compare_digest(
-        correct_password, credentials.password
-    ):
+    is_correct_user = secrets.compare_digest(
+        credentials.username,
+        settings.API_USER,
+    )
+
+    is_correct_password = secrets.compare_digest(
+        credentials.password,
+        settings.API_PASSWORD,
+    )
+
+    if not (is_correct_user and is_correct_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid credentials",
